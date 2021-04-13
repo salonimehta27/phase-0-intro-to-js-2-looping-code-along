03783503504bd3e45a6d29e7ee6b06dfad1fb784
Saloni <salonimehta2127@gmail.com>
2021-04-12 23:39:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const names = [["lisa", "kaitlin", "jan"], "surprise"];
+
+function writeCards(names,event){
+    for (let i =0; i < names.length; i++)
+    {
+        names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+
+    }
+     return names;
+
+}
+writeCards(["lisa", "kaitlin", "jan"], "surprise");
+
+const gifts=["teddy","drone","doll"];
+
+function countDown(i)
+{
+    while(i>=0)
+    {
+        console.log(i--);
+    }
+
+}
+countDown(10);
+
