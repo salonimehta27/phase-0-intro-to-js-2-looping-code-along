574a7235093efa90616ea875d02a70812d7e4db2
Saloni <salonimehta2127@gmail.com>
2021-05-03 19:46:02 -0400

    commit tree 820f80722d75693ab351d3796e5f71c8e59c81ab

--- index.js
+++ index.js
@@ -1,26 +1,88 @@
-const names = [["lisa", "kaitlin", "jan"], "surprise"];
+//new attempt
 
-function writeCards(names,event){
-    for (let i =0; i < names.length; i++)
-    {
-        names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
-
-    }
-     return names;
-
-}
-writeCards(["lisa", "kaitlin", "jan"], "surprise");
-
-const gifts=["teddy","drone","doll"];
-
-function countDown(i)
+const names=['Lisa', 'Kaitlin', 'Jan'];
+const eventName='surprise';
+function writeCards(names,eventName)
 {
-    while(i>=0)
+for(let i=0;i<names.length;i++)
+{
+names[i]=(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+debugger;
+}
+return names;
+}
+writeCards(names,eventName);
+
+function countDown()
+{
+    let countDown=10;
+    while(countDown>=0)
     {
-        console.log(i--);
+        console.log(countDown--);
     }
 
 }
-countDown(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old attempt from
+
+
+
+
+// const names = [["lisa", "kaitlin", "jan"], "surprise"];
+
+// function writeCards(names,event){
+//     for (let i =0; i < names.length; i++)
+//     {
+//         names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+
+//     }
+//      return names;
+
+// }
+// writeCards(["lisa", "kaitlin", "jan"], "surprise");
+
+// const gifts=["teddy","drone","doll"];
+
+// function countDown(i)
+// {
+//     while(i>=0)
+//     {
+//         console.log(i--);
+//     }
+
+// }
+// countDown(10);
 
 
